91103f81affb241b7b8a0c53252878a684231587
EquinoxTheGryph <18143080+EquinoxTheGryph@users.noreply.github.com>
2024-12-15 15:29:30 +0100

    Add some information

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -1,11 +1,15 @@
 <script>
     import Button88x31 from '$components/Button88x31.svelte';
-    
-    export let data
+
+    export let data;
 </script>
 
 <article class="flex flex-col items-center p-10">
     <h1 class="c-text">Buttons!</h1>
+    <p class="c-text text-center">
+        Some random buttons I have collected over a period of time. These are not in any particular
+        order.
+    </p>
     <div class="m-5 flex max-w-[40em] flex-wrap justify-center gap-2">
         {#each data.buttons as button}
             <Button88x31 value={button} />
@@ -13,6 +17,13 @@
     </div>
 
     <h2 class="c-text">Feel free to use mine!</h2>
+    <p class="c-text text-center mb-3">
+        Featuring various art pieces found on my <a
+            href="https://www.furaffinity.net/gallery/aphelion1"
+        >
+            FA Gallery!
+        </a>
+    </p>
     <Button88x31
         value={{
             url: '/images/EquiRocks88x31.gif',
