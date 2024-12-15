e866a0318b9a3c37d7e1fbb8076977c2c55ac1b3
EquinoxTheGryph <18143080+EquinoxTheGryph@users.noreply.github.com>
2024-12-15 15:49:35 +0100

    Small style update

--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -7,7 +7,7 @@
 
 <article class="flex flex-col items-center p-10">
     <h1 class="c-text">My Characters</h1>
-    <p class="c-text text-center">
+    <p class="c-text text-center mb-4">
         Click on the <Icon path={mdiImageSearchOutline} class="inline" size="1em" title="Image Icon" /> to go to the character's
         reference sheet
     </p>
